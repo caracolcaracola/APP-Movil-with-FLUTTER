5f16b3620ab56cfc3f9e348b8370eafaeb2dd7ca
caracolcaracola <tobias.suarez.t1vl@gmail.com>
2023-10-06 09:24:28 +0100

    change

---
+++ appmovil/lib/models/detalles_pedidos.dart
@@ -0,0 +1,47 @@
+
+// class DetallesPedidos {
+//   int? id;
+//   int? idPedido;
+//   // Null? idHerramienta;
+//   String? herramienta;
+//   int? cantidadSolicitada;
+//   // Null? estado;
+//   // Null? inventario;
+//   // Null? devoluciones;
+
+//   DetallesPedidos(
+//       {this.id,
+//       this.idPedido,
+//       // this.idHerramienta,
+//       this.herramienta,
+//       this.cantidadSolicitada,
+//       // this.estado,
+//       // this.inventario,
+//       // this.devoluciones
+//       });
+
+//   DetallesPedidos.fromJson(Map<String, dynamic> json) {
+//     id = json['id'];
+//     idPedido = json['id_pedido'];
+//     // idHerramienta = json['id_herramienta'];
+//     herramienta = json['herramienta'];
+//     cantidadSolicitada = json['cantidad_solicitada'];
+//     // estado = json['estado'];
+//     // inventario = json['inventario'];
+//     // devoluciones = json['devoluciones'];
+//   }
+
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['id'] = this.id;
+//     data['id_pedido'] = this.idPedido;
+//     // data['id_herramienta'] = this.idHerramienta;
+//     data['herramienta'] = this.herramienta;
+//     data['cantidad_solicitada'] = this.cantidadSolicitada;
+//     // data['estado'] = this.estado;
+//     // data['inventario'] = this.inventario;
+//     // data['devoluciones'] = this.devoluciones;
+//     return data;
+//   }
+// }
+ 
---
+++ appmovil/lib/models/inventario.dart
@@ -0,0 +1,47 @@
+
+// class Inventario {
+//   String? herramienta;
+//   int? cantidad;
+//   String? rubro;
+//   String? subRubro;
+//   String? proveedor;
+//   String? ubicacion;
+//   // Null? idDetalle;
+//   int? id;
+
+//   Inventario(
+//       {this.herramienta,
+//       this.cantidad,
+//       this.rubro,
+//       this.subRubro,
+//       this.proveedor,
+//       this.ubicacion,
+//       // this.idDetalle,
+//       this.id});
+
+//   Inventario.fromJson(Map<String, dynamic> json) {
+//     herramienta = json['herramienta'];
+//     cantidad = json['cantidad'];
+//     rubro = json['rubro'];
+//     subRubro = json['sub_rubro'];
+//     proveedor = json['proveedor'];
+//     ubicacion = json['ubicacion'];
+//     // idDetalle = json['id_detalle'];
+//     id = json['id'];
+//   }
+
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['herramienta'] = this.herramienta;
+//     data['cantidad'] = this.cantidad;
+//     data['rubro'] = this.rubro;
+//     data['sub_rubro'] = this.subRubro;
+//     data['proveedor'] = this.proveedor;
+//     data['ubicacion'] = this.ubicacion;
+//     // data['id_detalle'] = this.idDetalle;
+//     data['id'] = this.id;
+//     return data;
+//   }
+// }
+
+ 
