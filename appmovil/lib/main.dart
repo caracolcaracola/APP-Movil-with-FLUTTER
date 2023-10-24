73db8cdaa3475a3619571c483a10baba912551f3
IgnacioTDeter <detergithub@gmail.com>
2023-10-24 16:12:16 -0300

    Buscador y nombres de archivos
    Agregue los estilos para un buscador. Cambie los nombres de los archivos porque los otros no estan permitdos en flutter

--- appmovil/lib/main.dart
+++ appmovil/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'InventarioScreen.dart';
-import 'InformesScreen.dart';
+import 'Inventario_screen.dart';
+import 'Informes_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,11 +22,13 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Pañol App'),
       ),
+
+      // Menu "Hamburgesa"
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
+            const DrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
@@ -38,35 +40,101 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
+
             ListTile(
-              title: Text('Inventario'),
+              title: const Text('Inventario'),
               onTap: () {
                 Navigator.pop(context); // Cierra el Drawer
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) =>
-                          InventarioScreen()), // Navega a InventarioScreen
+                    builder: (context) => InventarioScreen(), // Navega a InventarioScreen
+                  ),
                 );
               },
             ),
+
             ListTile(
-              title: Text('Informes'),
+              title: const Text('Informes'),
               onTap: () {
                 Navigator.pop(context); // Cierra el Drawer
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) =>
-                          InformesScreen()), // Navega a InformesScreen
+                    builder: (context) => InformesScreen(), // Navega a InformesScreen
+                  ),
                 );
               },
             ),
           ],
         ),
       ),
-      body: Center(
-        child: Text('Contenido principal de la aplicación'),
+      // Fin de Menu "Hamburgesa"
+
+      body: 
+      
+      // Inicio "Bienvenido al Pañol"
+      Column(
+        children: <Widget>[
+         const Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Bienvenido al pañol",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          //Fin de "Bienvenido al Pañol"
+
+
+
+          //Inicio Buscador
+          /* Estilos de la "caja" */
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              
+              /* Estilos dentro de la caja*/ 
+              child: Row(
+                children: <Widget>[
+                 const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Buscar...',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(15.0),
+                      ),
+                    ),
+                  ),
+
+                  /* Estilos y logica del boton "Buscar" */
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      // Agrega aquí la lógica para realizar la búsqueda.
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
