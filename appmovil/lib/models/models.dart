46dc599efb496434e4ebdeedfc53caffb59b38a8
Joaquín Rodriguez Grossi <109248029+jaocorod@users.noreply.github.com>
2023-10-27 21:06:58 -0300

    Delete appmovil directory

--- appmovil/lib/models/models.dart
+++
@@ -1,5 +0,0 @@
-
-// export 'package:appmovil/models/detalles_pedidos.dart';
-// export 'package:appmovil/models/inventario.dart';
-// export 'package:appmovil/models/pedidos.dart';
-export 'package:appmovil/models/users.dart';
