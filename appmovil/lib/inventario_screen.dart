ce0c3f48d26d71859fd31c014541478f6dc270b5
IgnacioTDeter <detergithub@gmail.com>
2023-10-30 12:57:09 -0300

    Inventario BD
    El inventario ahora muestra informacion en tiempo real de la base de datos

--- appmovil/lib/inventario_screen.dart
+++ appmovil/lib/inventario_screen.dart
@@ -1,66 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class InventarioScreen extends StatelessWidget {
+class InventarioScreen extends StatefulWidget {
+  @override
+  _InventarioScreenState createState() => _InventarioScreenState();
+}
+
+class _InventarioScreenState extends State<InventarioScreen> {
+  List<Map<String, dynamic>> inventario = [];
+
+  @override
+  void initState() {
+    super.initState();
+    obtenerDatosDeInventario();
+  }
+
+  Future<void> obtenerDatosDeInventario() async {
+    final response = await http.get(Uri.parse('http://localhost/flutter/inventario.php'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        inventario = data.cast<Map<String, dynamic>>();
+      });
+    } else {
+      throw Exception('Error al obtener los datos de inventario');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<DataRow> rows = [];
+
+    for (int i = 0; i < inventario.length; i++) {
+      final item = inventario[i];
+
+      DataRow row = DataRow(
+        cells: <DataCell>[
+          DataCell(Text(item['herramienta'])),
+          DataCell(Text(item['cantidad'].toString())),
+          DataCell(Text(item['ubicacion'])),
+        ],
+      );
+
+      rows.add(row);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pantalla de Inventario'),
       ),
-      body: Table(
-      border: TableBorder.all(),
-      columnWidths: const <int, TableColumnWidth>{
-        0: IntrinsicColumnWidth(),
-        1: FlexColumnWidth(),
-        2: FixedColumnWidth(64),
-      },
-      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-      children: <TableRow>[
-        TableRow(
-          children: <Widget>[
-            Container(
-              height: 32,
-              color: Colors.green,
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: DataTable(
+              headingRowColor: MaterialStateColor.resolveWith((states) {
+                return Color(0xFF393F46);
+              }),
+              columns: const <DataColumn>[
+                DataColumn(
+                  label: Text(
+                    'Nombre',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Cantidad',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Ubicación',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+              rows: rows,
             ),
-            TableCell(
-              verticalAlignment: TableCellVerticalAlignment.top,
-              child: Container(
-                height: 32,
-                width: 32,
-                color: Colors.red,
-              ),
-            ),
-            Container(
-              height: 64,
-              color: Colors.blue,
-            ),
-          ],
-        ),
-        TableRow(
-          decoration: const BoxDecoration(
-            color: Colors.grey,
           ),
-          children: <Widget>[
-            Container(
-              height: 64,
-              width: 128,
-              color: Colors.purple,
-            ),
-            Container(
-              height: 32,
-              color: Colors.yellow,
-            ),
-            Center(
-              child: Container(
-                height: 32,
-                width: 32,
-                color: Colors.orange,
-              ),
-            ),
-          ],
         ),
-      ],
-    )
+      ),
     );
   }
 }
